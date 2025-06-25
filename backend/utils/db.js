d0251415c3f9a47ae893b954750046343ec126a8
Muhammad Farhan <137469472+muhhfarhann@users.noreply.github.com>
2025-06-25 13:51:12 +0700

    Delete backend directory

--- backend/utils/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-const urlMongodbAtlas = process.env.MONGO_URI;
-
-mongoose.connect(urlMongodbAtlas)
-    .then(() => console.log('✅ Koneksi MongoDB berhasil'))
-    .catch(err => console.error('❌ Koneksi MongoDB gagal:', err));
