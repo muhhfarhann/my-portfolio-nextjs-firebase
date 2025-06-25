4f8fc9407417b93457566183675488156f414998
Muhammad Farhan <137469472+muhhfarhann@users.noreply.github.com>
2025-06-25 13:51:36 +0700

    Delete frontend directory

--- frontend/src/components/home/Header.jsx
+++
@@ -1,8 +0,0 @@
-// element header
-export default function HeaderContainer({ children }) {
-  return (
-    <header className="px-5 relative" id="beranda">
-      {children}
-    </header>
-  );
-}
--- frontend/src/pages/NotFound.jsx
+++
@@ -1,5 +0,0 @@
-export default function NotFound() {
-  return (
-    <h1 className="text-center font-extrabold text-5xl">404 Not Found Bro!</h1>
-  );
-}
